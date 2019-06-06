cd77b2f66cc97215ae9e9957ea08ffafc0f13017
cperez26 <42821682+cperez26@users.noreply.github.com>
2019-06-06 18:40:31 -0500

    tercer commit (reestructuracion)

--- tarea.js
+++ tarea.js
@@ -1,41 +1,31 @@
+
 export default class Tarea {
-    constructor(tarea) {
-      this._nombre = tarea.nombre.toUpperCase();
+  constructor(tarea) {
+      this._nombre = tarea.nombre;
       this._fechaFin = tarea.fechaFin;
-      this._months = [
-        "Ene", "Feb",
-        "Mar", "Abr",
-        "May", "Jun",
-        "Jul", "Ago",
-        "Sep", "Oct",
-        "Nov", "Dic"
-      ];
-    }
+  }
 
   get nombre() {
-    return this._nombre;
+      return this._nombre;
   }
 
   get fechaFin() {
-    return this._fechaFin;
+      return this._fechaFin;
   }
 
   getDateAsString() {
-    let fecha =
-      this._fechaFin.getDate() +
-      "/" +
-      this._months[this._fechaFin.getMonth()] +
-      "/" +
-      this._fehcaFin.getFullYear();
-    return fecha;
+      let d = this._fechaFin.getDate() 
+      + "/" + 
+      this._fechaFin.getMonth() 
+      + "/" + 
+      this._fechaFin.getFullYear();
+      return d;
   }
-  
+
   getDias() {
-    let oneDay = 24 * 60 * 60 * 1000;
-    let oneYear = oneDay * 365;
-    let differenceMs = new Date() - this._fechaFin;
-    let dias = Math.trunc(differenceMs / oneYear);
-  
-    return dias;
+      let oneDay = (24 * 60 * 60 * 1000);
+
+      let differenceMs = Math.abs(new Date() - this._fechaFin);
+      return Math.round(differenceMs / oneDay);
   }
 }
